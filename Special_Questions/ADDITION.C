564e5c58bdc01d13843f5d80cad713356d4aed2d
Ankit Kumar <118204626+AnkitKumaarr@users.noreply.github.com>
2023-01-18 23:02:33 +0530

    Add files via upload

---
+++ Special_Questions/ADDITION.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+	int a =12,  b = 5,rem,res,div;
+	clrscr();
+	while(b!=0){
+	 b--;
+	 a++;
+	}
+	printf("The addition is -- %d",a);
+
+ getch();
+}
